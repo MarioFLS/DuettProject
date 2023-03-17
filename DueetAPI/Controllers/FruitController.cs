4c768447e6f1b7ddef64c7d3617978782af31008
MarioFLS <mariofernandolsantos@gmail.com>
2023-03-16 23:15:00 -0300

    feat: add controllers fruits

---
+++ DueetAPI/Controllers/FruitController.cs
@@ -0,0 +1,26 @@
+﻿using DueetAPI.Models;
+using DueetAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DueetAPI.Controllers
+{
+    [Route("api/fruit")]
+    [ApiController]
+    public class FruitController : ControllerBase
+    {
+        private readonly IDueetRepository _dueetRepository;
+
+        public FruitController(IDueetRepository dueetRepository)
+        {
+            _dueetRepository = dueetRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Fruit>>> FindAll()
+        {
+            var fruits = await _dueetRepository.FindAll();
+            return Ok(fruits);
+        }
+    }
+}
